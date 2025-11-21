7af432475ee0320f3d933127d0cdf89a0e2ef58e
Samuel <samueelrs979@gmail.com>
2025-11-21 15:05:12 -0300

    new changes made to project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-
+const workersSetting = process.env.CI ? { workers: 1 } : {};
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+...workersSetting,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
